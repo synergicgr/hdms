df7324a55a2c046055d56c58364b638013929449
synergicgr <admin@synergic.gr>
2018-10-31 17:16:20 +0200

    Added Google Maps

--- src/pages/google-maps/google-maps.ts
+++ src/pages/google-maps/google-maps.ts
@@ -1,6 +1,16 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+import {
+  GoogleMaps,
+  GoogleMap,
+  GoogleMapsEvent,
+  GoogleMapOptions,
+  CameraPosition,
+  MarkerOptions,
+  Marker,
+  Environment
+} from '@ionic-native/google-maps';
 /**
  * Generated class for the GoogleMapsPage page.
  *
@@ -15,11 +25,44 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class GoogleMapsPage {
 
+  map: GoogleMap;
+
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.loadMap();
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad GoogleMapsPage');
-  }
+  loadMap(): void {
 
+    /*
+    // This code is necessary for browser
+    Environment.setEnv({
+      'API_KEY_FOR_BROWSER_RELEASE': '(your api key for `https://`)',
+      'API_KEY_FOR_BROWSER_DEBUG': '(your api key for `http://`)'
+    });
+    */
+
+    let mapOptions: GoogleMapOptions = {
+      camera: {
+        target: {
+          lat: 37.955616,
+          lng: 23.854213
+        },
+        zoom: 18,
+        tilt: 30
+      }
+    };
+
+    this.map = GoogleMaps.create('map', mapOptions);
+
+    let marker: Marker = this.map.addMarkerSync({
+      title: 'Hellas DMS',
+      icon: 'orange',
+      animation: 'DROP',
+      position: {
+        lat: 37.955616,
+        lng: 23.854213
+      }
+    });    
+  }
 }
