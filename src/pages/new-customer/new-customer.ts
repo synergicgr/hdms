ddb9938516c4fb587b334053cd81ad588dd7a6fa
synergicgr <admin@synergic.gr>
2018-11-06 12:47:22 +0200

    Added add functionality in customers and zones

--- src/pages/new-customer/new-customer.ts
+++ src/pages/new-customer/new-customer.ts
@@ -8,21 +8,21 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class NewCustomerPage {
 
-  phoneNotices: Array<{ name: string, phone: string }> = [
-    { name: "Ελένη Γεωργίου", phone: "211 45 55 456" },
-    { name: "Βαγγέλης Γεωργίου", phone: "687 64 52 354" },
-    { name: "Αγγελική Γεωργίου", phone: "687 64 52 354" },
+  phoneNotices: Array<{ name: string, phone: string, editable:boolean}> = [
+    { name: "Ελένη Γεωργίου", phone: "211 45 55 456", editable:false },
+    { name: "Βαγγέλης Γεωργίου", phone: "687 64 52 354", editable:false },
+    { name: "Αγγελική Γεωργίου", phone: "687 64 52 354", editable:false },
   ];
 
-  zones: Array<{ name: string, id: string }> = [
-    { name: "ΖΩΝΗ 1", id: "ΑΒ1128336" },
-    { name: "ΖΩΝΗ 2", id: "ΑΒ1128336" },
-    { name: "ΖΩΝΗ 3", id: "ΑΒ1128336" },
-    { name: "ΖΩΝΗ 4", id: "ΑΒ1128336" },
-    { name: "ΖΩΝΗ 5", id: "ΑΒ1128336" },
-    { name: "ΖΩΝΗ 6", id: "ΑΒ1128336" },
-    { name: "ΖΩΝΗ 7", id: "ΑΒ1128336" },
-    { name: "ΖΩΝΗ 8", id: "ΑΒ1128336" },
+  zones: Array<{ name: string, id: string, editable:boolean }> = [
+    { name: "ΖΩΝΗ 1", id: "ΑΒ1128336", editable:false },
+    { name: "ΖΩΝΗ 2", id: "ΑΒ1128336" , editable:false },
+    { name: "ΖΩΝΗ 3", id: "ΑΒ1128336" , editable:false },
+    { name: "ΖΩΝΗ 4", id: "ΑΒ1128336" , editable:false },
+    { name: "ΖΩΝΗ 5", id: "ΑΒ1128336" , editable:false },
+    { name: "ΖΩΝΗ 6", id: "ΑΒ1128336" , editable:false },
+    { name: "ΖΩΝΗ 7", id: "ΑΒ1128336" , editable:false },
+    { name: "ΖΩΝΗ 8", id: "ΑΒ1128336" , editable:false },
   ];
 
   alarmUsers: Array<{ username: string, name: string }> = [
@@ -39,4 +39,23 @@ export class NewCustomerPage {
     console.log('ionViewDidLoad NewCustomerPage');
   }
 
+  removeZone(index):void{
+    this.zones.splice(index, 1);
+  }
+
+  removeAlarmUser(index):void{
+    this.alarmUsers.splice(index, 1);
+  }
+
+  removePhoneNotice(index):void{
+    this.phoneNotices.splice(index, 1);
+  }
+
+  addPhoneNotices():void{
+    this.phoneNotices.push({ name: "", phone: "", editable:true });
+  }
+
+  addZone():void{
+    this.zones.push({ name: "", id: "", editable:true });
+  }
 }
