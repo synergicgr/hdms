87f9cf6365061c01f1a96341a8e188ad84bd0a89
synergicgr <admin@synergic.gr>
2018-11-06 18:24:56 +0200

    Changed active/inactive tabs icon color

--- src/pages/google-maps/google-maps.ts
+++ src/pages/google-maps/google-maps.ts
@@ -23,46 +23,9 @@ import {
   selector: 'page-google-maps',
   templateUrl: 'google-maps.html',
 })
-export class GoogleMapsPage {
-
-  map: GoogleMap;
-
+export class GoogleMapsPage {  
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-    //this.loadMap();
   }
-
-  loadMap(): void {
-
-    /*
-    // This code is necessary for browser
-    Environment.setEnv({
-      'API_KEY_FOR_BROWSER_RELEASE': '(your api key for `https://`)',
-      'API_KEY_FOR_BROWSER_DEBUG': '(your api key for `http://`)'
-    });
-    */
-
-    let mapOptions: GoogleMapOptions = {
-      camera: {
-        target: {
-          lat: 37.955616,
-          lng: 23.854213
-        },
-        zoom: 18,
-        tilt: 30
-      }
-    };
-
-    this.map = GoogleMaps.create('map', mapOptions);
-
-    let marker: Marker = this.map.addMarkerSync({
-      title: 'Hellas DMS',
-      icon: 'orange',
-      animation: 'DROP',
-      position: {
-        lat: 37.955616,
-        lng: 23.854213
-      }
-    });    
-  }
+   
 }
