54a67aecf7476a54b8f42cf4ab8acabb7dd84371
synergicgr <admin@synergic.gr>
2018-11-07 10:08:51 +0200

    Added PopOver in Notes Page

--- src/pages/notes/notes.ts
+++ src/pages/notes/notes.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, Platform } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, Platform, PopoverController } from 'ionic-angular';
 import { DashboardPage } from '../dashboard/dashboard';
+import { PopOverPage } from '../pop-over/pop-over';
 
 
 /**
@@ -17,20 +18,41 @@ import { DashboardPage } from '../dashboard/dashboard';
 })
 export class NotesPage {
 
+  private popover;
+  open:boolean;
+
   notes: Array<{ creationDate:string, showDate: string, content: string}> = [
     {creationDate:new Date().toDateString(), showDate: "2018-12-12 15:00", content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere, ipsum non facilisis luctus, libero urna ullamcorper erat, nec suscipit nisl dui id justo. Sed venenatis congue mi sed consectetur. Proin et nisl ac enim fringilla facilisis. Vestibulum posuere mi eget pulvinar sollicitudin. Aliquam lacus elit, venenatis eget risus a, aliquam suscipit magna. Morbi sem nunc, feugiat id nulla at, pretium tincidunt tortor. Quisque porta eget mauris vitae finibus. Curabitur libero sapien, lobortis at sodales ut, pulvinar et turpis. Nulla fringilla lectus id ultricies faucibus. Curabitur vel varius augue. Praesent iaculis nec augue nec finibus."},
     {creationDate:new Date().toDateString(), showDate: "2018-12-12 15:00", content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere, ipsum non facilisis luctus, libero urna ullamcorper erat, nec suscipit nisl dui id justo. Sed venenatis congue mi sed consectetur. Proin et nisl ac enim fringilla facilisis. Vestibulum posuere mi eget pulvinar sollicitudin. Aliquam lacus elit, venenatis eget risus a, aliquam suscipit magna. Morbi sem nunc, feugiat id nulla at, pretium tincidunt tortor. Quisque porta eget mauris vitae finibus. Curabitur libero sapien, lobortis at sodales ut, pulvinar et turpis. Nulla fringilla lectus id ultricies faucibus. Curabitur vel varius augue. Praesent iaculis nec augue nec finibus."}
   ];
 
-
-  constructor(public navCtrl: NavController, public navParams: NavParams, private platform:Platform) {    
-    this.platform.registerBackButtonAction(() => {
-      this.navCtrl.setRoot(DashboardPage);
+  constructor(public navCtrl: NavController, public navParams: NavParams,     public popoverCtrl: PopoverController, private platform:Platform) {    
+    platform.ready().then(() => {
+      platform.registerBackButtonAction(() => {
+        if(this.open === true)
+        {
+          this.popover.dismiss();
+          this.open = false;
+        }
+        else{
+          navCtrl.setRoot(DashboardPage);
+        }        
+      });
     });
+  }
+
+  presentPopover(myEvent) {
+    this.popover = this.popoverCtrl.create(PopOverPage);
+    this.popover.present({
+      ev: myEvent,
+    });
+    this.open = true;    
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad NotesPage');
   }
+
+
 
 }
