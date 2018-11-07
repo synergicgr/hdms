f0b024a715e9b83aaa6378f27ad5357953b3cca2
synergicgr <admin@synergic.gr>
2018-11-07 15:04:18 +0200

    Added ngModel in Installer Details Page

--- src/pages/installer-details/installer-details.ts
+++ src/pages/installer-details/installer-details.ts
@@ -14,6 +14,22 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'installer-details.html',
 })
 export class InstallerDetailsPage {
+  
+  name:string;
+  afm:string;
+  proffesionalDescription:string;
+  insuredAreaAddress:string;
+  insuredAreaCity:string;
+  insuredAreaPostCode:string;
+  insuredAreaFloor:string;
+  landlinePhone:string;
+  mobilePhone:string;
+  email:string;
+  website:string;
+  collectionPolicy:string;
+  emailInvoice:string;
+  billingAddressOnly:string;
+
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
