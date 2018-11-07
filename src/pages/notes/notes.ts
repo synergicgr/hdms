6bff1d6a94d86432bee18e4be4d7355979f0a8f5
synergicgr <admin@synergic.gr>
2018-11-07 18:34:11 +0200

    Add round borders in notes

--- src/pages/notes/notes.ts
+++ src/pages/notes/notes.ts
@@ -24,7 +24,8 @@ export class NotesPage {
 
   notes: Array<{ creationDate:string, showDate: string, content: string}> = [
     {creationDate:new Date().toDateString(), showDate: "2018-12-12 15:00", content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere, ipsum non facilisis luctus, libero urna ullamcorper erat, nec suscipit nisl dui id justo. Sed venenatis congue mi sed consectetur. Proin et nisl ac enim fringilla facilisis. Vestibulum posuere mi eget pulvinar sollicitudin. Aliquam lacus elit, venenatis eget risus a, aliquam suscipit magna. Morbi sem nunc, feugiat id nulla at, pretium tincidunt tortor. Quisque porta eget mauris vitae finibus. Curabitur libero sapien, lobortis at sodales ut, pulvinar et turpis. Nulla fringilla lectus id ultricies faucibus. Curabitur vel varius augue. Praesent iaculis nec augue nec finibus."},
-    {creationDate:new Date().toDateString(), showDate: "2018-12-12 15:00", content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere, ipsum non facilisis luctus, libero urna ullamcorper erat, nec suscipit nisl dui id justo. Sed venenatis congue mi sed consectetur. Proin et nisl ac enim fringilla facilisis. Vestibulum posuere mi eget pulvinar sollicitudin. Aliquam lacus elit, venenatis eget risus a, aliquam suscipit magna. Morbi sem nunc, feugiat id nulla at, pretium tincidunt tortor. Quisque porta eget mauris vitae finibus. Curabitur libero sapien, lobortis at sodales ut, pulvinar et turpis. Nulla fringilla lectus id ultricies faucibus. Curabitur vel varius augue. Praesent iaculis nec augue nec finibus."}
+    {creationDate:new Date().toDateString(), showDate: "2018-12-12 15:00", content:"Testing 1"},
+    {creationDate:new Date().toDateString(), showDate: "2018-12-12 15:00", content:"Testing 2"}
   ];
 
   constructor(public navCtrl: NavController, public navParams: NavParams,     public popoverCtrl: PopoverController, private platform:Platform) {    
@@ -51,7 +52,8 @@ export class NotesPage {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad NotesPage');
+    console.log('ionViewDidLoad NotesPage');     
+    console.log(this.notes.slice(1,-1));
   }
 
   openNote(index):void{
